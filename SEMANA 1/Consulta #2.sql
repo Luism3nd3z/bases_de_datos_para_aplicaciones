10b3a1169444ce432bf14296b7c58dc596004dda
Luism3nd3z <78186488+Luism3nd3z@users.noreply.github.com>
2021-05-16 18:37:57 -0500

    Add files via upload

---
+++ SEMANA 1/Consulta #2.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO usuarios 
+VALUES ('1','3','8','Luis Mendez','alto moreno','2020-05-11 22-10-59'),
+('2','4','6','jose antonio','moreno','2020-01-02 11-45-01'),
+('3','9','0','Brandon osmar','alto ','2020-12-05 05-34-54'),
+('4','0','12','jhonathan contreras','moreno','2020-09-09 01-21-34');
---
+++ SEMANA 1/Consulta #3.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO productos
+VALUES ('1','computadora','computadora de escritorio-intel celeron-4GB-RAM500','1500.3','50'),
+('2','CPU','7.2 GHz-4 GB-HDMI','1000.9','2'),
+('3','teclado','teclado para pc Qwerty logitech k120','500.6','20'),
+('4','mouse','teclado para pc Qwerty logitech k120','100.0','35');
