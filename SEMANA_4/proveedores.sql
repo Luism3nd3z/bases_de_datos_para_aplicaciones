e19db87281ec74a56ada87558cac5f3f7e37d2c0
Luism3nd3z <78186488+Luism3nd3z@users.noreply.github.com>
2021-06-06 22:43:31 -0500

    Add files via upload

---
+++ SEMANA_4/proveedores.sql
@@ -0,0 +1,8 @@
+
+USE tienda_acme;
+INSERT INTO proveedores 
+VALUES (1,"Adobe","Ingrid-Stein"," +1 646 980 4741","dignissim.lacus@nasceturridiculus.org"," 2020-11-11 15-12-12 "),
+(2,"Chami","Maxwell-Guerra","+52 55 710 031 12  ","Donec@nuncid.edu"," 2020-04-20 06-32-18 "),
+(3,"Altavista","Paloma-Weaver","+52 55 710 031 12  ","et.malesuada@tempus.ca"," 2021-06-01 17-31-15 "),
+(4,"Chami","Tyrone-Rivas"," +1 646 980 4741","auctor.Mauris.vel@diameudolor.net"," 2021-05-20 07-41-13 "),
+(5,"Macromedia","Jaden-Dickson","+52 55 710 031 12  ","libero.mauris.aliquam@Donecelementumlorem.org","  2020-09-19 20-34-53 ");
